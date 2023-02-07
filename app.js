2a4e353c18bf48eb0a1a863b7c9b8871c1143d6c
Rohan Chugh <rohanchugh14@gmail.com>
2023-02-06 23:03:56 -0600

    updated welcome message

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 3001;
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () =>{
- console.log(`Example app listening on port ${port}!`)
+ console.log(`Website backend and proxy listening on port ${port}!`)
 });
 
 // setup a request at /testing that will subsequently make an http GET request to
